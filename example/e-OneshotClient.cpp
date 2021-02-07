ba32853dce1096311644fc29348cba646e125572
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-02-07 16:44:31 +0200

    Oneshot client demo

---
+++ example/e-OneshotClient.cpp
@@ -0,0 +1,43 @@
+#include <fishnets/WebSocketClient.hpp>
+#include <fishnets/WebSocketSession.hpp>
+
+#include <iostream>
+
+class OneshotSession final : public fishnets::WebSocketSession
+{
+    void wsOpened() override
+    {
+        std::cout << "Connected\n";
+        m_sent = "cool message";
+        wsSend(m_sent);
+    }
+
+    void wsClosed() override
+    {
+        std::cout << "Disconnected\n";
+    }
+
+    void wsReceivedBinary(itlib::const_memory_view<uint8_t> binary) override
+    {
+        std::cout << "Received binary with size " << binary.size() << '\n';
+        wsClose();
+    }
+
+    void wsReceivedText(std::string_view text) override
+    {
+        std::cout << "Received text " << text << '\n';
+        wsClose();
+    }
+
+    void wsCompletedSend() override {}
+
+    std::string m_sent;
+};
+
+int main()
+{
+    //fishnets::WebSocketClient client(std::make_shared<OneshotSession>(), "echo.websocket.org", 443);
+    fishnets::WebSocketClient client(std::make_shared<OneshotSession>(), "localhost", 7654);
+
+    return 0;
+}
