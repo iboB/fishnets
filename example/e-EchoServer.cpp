878bf962ee3bd72a77ccfdb3391c4938f991c904
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-02-07 19:41:27 +0200

    License info in examples

--- example/e-EchoServer.cpp
+++ example/e-EchoServer.cpp
@@ -1,3 +1,10 @@
+// fishnets
+// Copyright (c) 2021 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
 #include <fishnets/WebSocketServer.hpp>
 #include <fishnets/WebSocketSession.hpp>
 #include <fishnets/WebSocketServerSSLSettings.hpp>
--- example/e-OneshotClient.cpp
+++ example/e-OneshotClient.cpp
@@ -1,3 +1,10 @@
+// fishnets
+// Copyright (c) 2021 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
 #include <fishnets/WebSocketClient.hpp>
 #include <fishnets/WebSocketClientSSLSettings.hpp>
 #include <fishnets/WebSocketSession.hpp>
