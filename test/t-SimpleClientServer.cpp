6f440bf34ae8eb543fd05c832c1173e4b125fdb1
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-02-08 09:12:43 +0200

    Simple test

--- test/t-SimpleClientServer.cpp
+++ test/t-SimpleClientServer.cpp
@@ -12,7 +12,90 @@
 #include <fishnets/WebSocketServer.hpp>
 #include <fishnets/WebSocketSession.hpp>
 
-class CounterClientSession : public fishnets::WebSocketSession
+struct Packet
+{
+    bool istext = false;
+    std::string text;
+    std::vector<uint8_t> binary;
+
+    bool operator==(std::string_view str) const
+    {
+        if (!istext) return false;
+        return text == str;
+    }
+
+    bool operator==(itlib::const_memory_view<uint8_t> bin) const
+    {
+        if (istext) return false;
+        if (binary.size() != bin.size()) return false;
+        return std::memcmp(binary.data(), bin.data(), binary.size()) == 0;
+    }
+};
+
+const std::vector<Packet> packets = {
+    Packet{true, "client 0", {}},
+    Packet{true, "client 1", {}},
+    Packet{false, {}, {1, 2, 3}},
+    Packet{false, {}, {5, 6, 7}},
+};
+
+class TestClientSession final : public fishnets::WebSocketSession
+{
+    void sendNext()
+    {
+        auto& packet = packets[sendIndex++];
+        if (packet.istext) wsSend(packet.text);
+        else wsSend(itlib::make_memory_view(packet.binary));
+    }
+
+    void closeIfDone()
+    {
+        if (sendIndex == packets.size() && receivedIndex == packets.size())
+        {
+            wsClose();
+        }
+    }
+
+    void wsOpened() override
+    {
+        sendNext();
+    }
+
+    void wsClosed() override
+    {
+    }
+
+    void wsReceivedBinary(itlib::const_memory_view<uint8_t> binary) override
+    {
+        REQUIRE(receivedIndex < packets.size());
+        CHECK(packets[receivedIndex] == binary);
+        ++receivedIndex;
+        closeIfDone();
+    }
+
+    void wsReceivedText(std::string_view text) override
+    {
+        REQUIRE(receivedIndex < packets.size());
+        CHECK(packets[receivedIndex] == text);
+        ++receivedIndex;
+        closeIfDone();
+    }
+
+    void wsCompletedSend() override
+    {
+        if (sendIndex == packets.size())
+        {
+            closeIfDone();
+            return;
+        }
+        sendNext();
+    }
+
+    size_t sendIndex = 0;
+    size_t receivedIndex = 0;
+};
+
+class TestServerSession final : public fishnets::WebSocketSession
 {
     void wsOpened() override
     {
@@ -24,18 +107,34 @@ class CounterClientSession : public fishnets::WebSocketSession
 
     void wsReceivedBinary(itlib::const_memory_view<uint8_t> binary) override
     {
+        REQUIRE(receivedIndex < packets.size());
+        CHECK(packets[receivedIndex] == binary);
+        ++receivedIndex;
+        wsSend(binary);
     }
 
     void wsReceivedText(std::string_view text) override
     {
+        REQUIRE(receivedIndex < packets.size());
+        CHECK(packets[receivedIndex] == text);
+        ++receivedIndex;
+        wsSend(text);
     }
 
     void wsCompletedSend() override
     {
     }
+
+    size_t receivedIndex = 0;
 };
+
+fishnets::WebSocketSessionPtr Make_ServerSession() { return std::make_shared<TestServerSession>(); }
 
 TEST_CASE("basic")
 {
+    const uint16_t port = 7654;
+    fishnets::WebSocketServer server(Make_ServerSession, port, 1, testServerSSLSettings.get());
 
+    auto clientSession = std::make_shared<TestClientSession>();
+    fishnets::WebSocketClient client(clientSession, "localhost", port, testClientSSLSettings.get());
 }
