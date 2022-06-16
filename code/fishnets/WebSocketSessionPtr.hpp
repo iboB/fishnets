0ad208d538b09b6c6dc0cd11995f03adffbfed7f
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-06-16 13:42:19 +0300

    Help with session factory func fwd declaration'

--- code/fishnets/WebSocketSessionPtr.hpp
+++ code/fishnets/WebSocketSessionPtr.hpp
@@ -13,4 +13,8 @@ namespace fishnets
 {
 class WebSocketSession;
 using WebSocketSessionPtr = std::shared_ptr<WebSocketSession>;
+
+// this is not really needed here per se,
+// but it's useful if you just want to forward declare session factories
+struct WebSocketEndpointInfo;
 }
