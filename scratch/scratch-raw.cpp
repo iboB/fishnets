9af1af164742eecccf71a15a3089f0387c7cf8a9
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-09-22 06:55:15 +0300

    Trimmed down obvious things

--- scratch/scratch-raw.cpp
+++ scratch/scratch-raw.cpp
@@ -33,18 +33,13 @@ using tcp = net::ip::tcp;
 
 class WebSocketSession;
 using WebSocketSessionPtr = std::shared_ptr<WebSocketSession>;
-struct WebSocketEndpointInfo;
 
 class Server;
-
-struct WebSocketEndpointInfo;
-using WebSocketSessionFactoryFunc = std::function<WebSocketSessionPtr(const WebSocketEndpointInfo&)>;
 
 class WebSocketServer
 {
 public:
     WebSocketServer(
-        WebSocketSessionFactoryFunc sessionFactory,
         uint16_t port,
         int numThreads = 1);
     ~WebSocketServer();
@@ -61,18 +56,18 @@ private:
 class Server;
 class SessionOwnerBase;
 class ExecutorHolder;
-struct WebSocketEndpointInfo;
-struct WebSocketSessionOptions;
 
-class WebSocketSession
+class WebSocketSession : public std::enable_shared_from_this<WebSocketSession>
 {
 public:
+    WebSocketSession();
+    ~WebSocketSession();
+
     WebSocketSession(const WebSocketSession&) = delete;
     WebSocketSession& operator=(const WebSocketSession&) = delete;
     WebSocketSession(WebSocketSession&&) noexcept = delete;
     WebSocketSession& operator=(WebSocketSession&&) noexcept = delete;
 
-    virtual WebSocketSessionOptions getInitialOptions();
     void postWSIOTask(std::function<void()> task);
     virtual void wsOpened();
     virtual void wsClosed();
@@ -85,17 +80,7 @@ public:
     void wsSend(std::string_view text);
     virtual void wsCompletedSend();
 
-    virtual void wsHeartbeat(uint32_t ms);
-
-    WebSocketEndpointInfo wsGetEndpointInfo() const;
-
-    void wsSetOptions(const WebSocketSessionOptions& options);
-
     std::string_view wsTarget() const;
-
-protected:
-    WebSocketSession();
-    ~WebSocketSession();
 
 private:
     friend class Server;
@@ -110,47 +95,14 @@ private:
     void closed();
 };
 
-struct WebSocketSessionOptions
-{
-    // id/name of host
-    // for servers this will be set as the "Server" HTTP header field
-    // for clients this will be set as the "User-Agent" HTTP header field
-    std::optional<std::string> hostId;
-
-    // max size of incoming message
-    // messages larger than that will be ignored
-    std::optional<size_t> maxIncomingMessageSize;
-
-    // timeout after which to disconnect when the other side doesn't respond
-    // note that this doesn't mean the time in which the other side hasn't communicated
-    // "not respoding" is based on pings which the library does internally
-    std::optional<std::chrono::milliseconds> idleTimeout;
-
-    // interval for wsHeartBeat. 0 means never
-    std::optional<std::chrono::milliseconds> heartbeatInterval;
-};
-
-struct WebSocketEndpointInfo
-{
-    std::string address;
-    uint16_t port = 0;
-};
-
 class ExecutorHolder
 {
 public:
-    ExecutorHolder(net::executor&& ex, const WebSocketSessionPtr& session)
+    ExecutorHolder(net::executor&& ex)
         : executor(std::move(ex))
-        , sessionSharedFromThis(session)
     {}
 
     net::executor executor;
-
-    // a poor man's shared-from this implementation, to avoid actually inheriting std::enable_shared_from_this in sessions
-    // and leave that (optional) inheritance to the user
-    // it's used to extend the lifetime of the session when wsio tasks are posted so that
-    // a plain [this] capture is possible in postWSIOTask
-    std::weak_ptr<WebSocketSession> sessionSharedFromThis;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -182,10 +134,6 @@ public:
     }
 
     virtual void acceptUpgrade() = 0;
-
-    // connect flow
-
-    virtual void connect(tcp::endpoint endpoint) = 0;
 
     // connections
 
@@ -250,12 +198,7 @@ public:
 
     // util
 
-    virtual WebSocketEndpointInfo getEndpointInfo() = 0;
-
-    virtual void setInitialServerOptions(WebSocketSessionOptions opts) = 0;
-    virtual void setInitialClientOptions(WebSocketSessionOptions opts) = 0;
-
-    virtual void setOptions(const WebSocketSessionOptions& opts) = 0;
+    virtual void setInitialServerOptions() = 0;
 
     void failed(beast::error_code e, const char* source)
     {
@@ -273,46 +216,9 @@ public:
         m_session = m_sessionPayload.get();
     }
 
-    void postHeartbeatTask(std::chrono::milliseconds ms)
-    {
-        m_heartbeatTimer->expires_after(ms);
-        m_heartbeatTimer->async_wait([wself = weak_from_this(), ms](beast::error_code e) {
-            if (e) return; // error
-            auto self = wself.lock();
-            if (!self) return; // destroyed
-            self->m_session->wsHeartbeat(uint32_t(ms.count()));
-
-            if (self->m_heartbeatTimer)
-            {
-                // only post another if the timer is sitll alive
-                // wsHeartbeat may have disabled it
-                self->postHeartbeatTask(ms);
-            }
-        });
-    }
-
-    void resetHeartbeatTimer(const std::optional<std::chrono::milliseconds>& duration)
-    {
-        auto ms = duration.value_or(std::chrono::milliseconds(0));
-        if (ms.count() == 0)
-        {
-            m_heartbeatTimer.reset();
-            return;
-        }
-
-        if (!m_heartbeatTimer)
-        {
-            m_heartbeatTimer.reset(new net::steady_timer(executor()));
-        }
-
-        postHeartbeatTask(ms);
-    }
-
     beast::flat_buffer m_readBuf;
     WebSocketSessionPtr m_sessionPayload;
     WebSocketSession* m_session = nullptr; // quick access pointer
-
-    std::unique_ptr<net::steady_timer> m_heartbeatTimer;
 
     // only relevant when accepting
     // cleared after the connection is established
@@ -334,11 +240,6 @@ WebSocketSession::WebSocketSession() = default;
 
 WebSocketSession::~WebSocketSession() = default;
 
-WebSocketSessionOptions WebSocketSession::getInitialOptions()
-{
-    return {};
-}
-
 void WebSocketSession::opened(SessionOwnerBase& session)
 {
     assert(!m_owner);
@@ -355,10 +256,10 @@ void WebSocketSession::closed()
 void WebSocketSession::postWSIOTask(std::function<void()> task)
 {
     net::dispatch(m_ioExecutorHolder->executor,
-        [self = m_ioExecutorHolder->sessionSharedFromThis.lock(), task = std::move(task)]() {
-        assert(self); // this can only fail if we're posting a taks in the session's destructor, which is definitely not a good idea
-        task();
-    }
+        [self = shared_from_this(), task = std::move(task)]() {
+            assert(self); // this can only fail if we're posting a taks in the session's destructor, which is definitely not a good idea
+            task();
+        }
     );
 }
 
@@ -398,20 +299,6 @@ void WebSocketSession::wsSend(std::string_view text)
 
 void WebSocketSession::wsCompletedSend() {}
 
-void WebSocketSession::wsHeartbeat(uint32_t) {}
-
-WebSocketEndpointInfo WebSocketSession::wsGetEndpointInfo() const
-{
-    if (!m_owner) return {};
-    return m_owner->getEndpointInfo();
-}
-
-void WebSocketSession::wsSetOptions(const WebSocketSessionOptions& options)
-{
-    if (!m_owner) return;
-    m_owner->setOptions(options);
-}
-
 std::string_view WebSocketSession::wsTarget() const
 {
     if (!m_owner) return {};
@@ -420,20 +307,6 @@ std::string_view WebSocketSession::wsTarget() const
 
 namespace
 {
-
-template <typename Socket>
-std::optional<WebSocketEndpointInfo> getEndpointInfoOf(const Socket& s)
-{
-    beast::error_code err;
-    auto ep = beast::get_lowest_layer(s).remote_endpoint(err);
-    // if there's an error, the socket has likely been disconnected
-    if (err) return {};
-
-    WebSocketEndpointInfo ret;
-    ret.address = ep.address().to_string();
-    ret.port = ep.port();
-    return ret;
-}
 
 template <typename WS>
 class SessionOwnerT : public SessionOwnerBase
@@ -483,91 +356,19 @@ public:
             beast::bind_front_handler(&SessionOwnerBase::onConnectionEstablished, shared_from_this()));
     }
 
-    // connect flow
-    void connect(tcp::endpoint endpoint) override final
-    {
-        beast::get_lowest_layer(m_ws).async_connect(endpoint,
-            beast::bind_front_handler(&SessionOwnerT::onConnectCB, shared_from_base()));
-    }
-
-    virtual void onConnectCB(beast::error_code e) = 0;
-
-    void onReadyForWSHandshake(beast::error_code e)
-    {
-        if (e) return failed(e, "ws connect");
-
-        setInitialClientOptions(m_session->getInitialOptions());
-
-        m_ws.async_handshake(m_host, m_target,
-            beast::bind_front_handler(&SessionOwnerBase::onConnectionEstablished, shared_from_this()));
-    }
-
     // util
-    WebSocketEndpointInfo getEndpointInfo() override final
+    void setInitialServerOptions() override final
     {
-        // if we end-up requesting tje enpoint info on a disconnected socket just return a default value
-        return getEndpointInfoOf(m_ws).value_or(WebSocketEndpointInfo{});
-    }
-
-    void setInitialServerOptions(WebSocketSessionOptions opts) override final
-    {
-        m_ws.read_message_max(opts.maxIncomingMessageSize.value_or(16 * 1024 * 1024));
+        m_ws.read_message_max(16 * 1024 * 1024);
 
         using bsb = websocket::stream_base;
         auto timeout = bsb::timeout::suggested(beast::role_type::server);
-        if (opts.idleTimeout) timeout.idle_timeout = *opts.idleTimeout;
         m_ws.set_option(timeout);
 
-        auto id = opts.hostId.value_or(
-            std::string("fishnets-ws-server ") + BOOST_BEAST_VERSION_STRING
-        );
+        auto id = std::string("ws-server ") + BOOST_BEAST_VERSION_STRING;
         m_ws.set_option(bsb::decorator([id = std::move(id)](websocket::response_type& res) {
             res.set(http::field::server, id);
         }));
-
-        resetHeartbeatTimer(opts.heartbeatInterval);
-    }
-
-    void setInitialClientOptions(WebSocketSessionOptions opts) override final
-    {
-        m_ws.read_message_max(opts.maxIncomingMessageSize.value_or(2 * 1024 * 1024));
-
-        using bsb = websocket::stream_base;
-        auto timeout = bsb::timeout::suggested(beast::role_type::client);
-        if (opts.idleTimeout) timeout.idle_timeout = *opts.idleTimeout;
-        m_ws.set_option(timeout);
-
-        auto id = opts.hostId.value_or(
-            std::string("fishnets-ws-client ") + BOOST_BEAST_VERSION_STRING
-        );
-        m_ws.set_option(bsb::decorator([id = std::move(id)](websocket::request_type& req) {
-            req.set(http::field::user_agent, id);
-        }));
-
-        resetHeartbeatTimer(opts.heartbeatInterval);
-    }
-
-    void setOptions(const WebSocketSessionOptions& opts) final override
-    {
-        // ignore hostId, as it's only valid when connecting
-
-        if (opts.maxIncomingMessageSize)
-        {
-            m_ws.read_message_max(*opts.maxIncomingMessageSize);
-        }
-
-        if (opts.idleTimeout)
-        {
-            websocket::stream_base::timeout t;
-            m_ws.get_option(t);
-            t.idle_timeout = *opts.idleTimeout;
-            m_ws.set_option(t);
-        }
-
-        if (opts.heartbeatInterval)
-        {
-            resetHeartbeatTimer(opts.heartbeatInterval);
-        }
     }
 };
 
@@ -599,12 +400,6 @@ public:
         http::async_read(m_ws.next_layer(), m_readBuf, m_upgradeRequest,
             beast::bind_front_handler(&SessionOwnerBase::onUpgradeRequest, shared_from_this()));
     }
-
-    // connect flow
-    void onConnectCB(beast::error_code e) override
-    {
-        onReadyForWSHandshake(e);
-    }
 };
 
 } // anonymous namespace
@@ -615,10 +410,9 @@ public:
 class Server
 {
 public:
-    Server(WebSocketSessionFactoryFunc sessionFactory, tcp::endpoint endpoint, int numThreads)
+    Server(tcp::endpoint endpoint, int numThreads)
         : m_ctx(numThreads)
         , m_acceptor(m_ctx, endpoint)
-        , m_sessionFactory(std::move(sessionFactory))
     {
         doAccept();
         m_threads.reserve(size_t(numThreads));
@@ -651,24 +445,12 @@ public:
         }
 
         // init session and owner
-        auto ep = getEndpointInfoOf(socket);
-        if (!ep)
-        {
-            std::cerr << "socket disconnected while accepting\n";
-            return;
-        }
-        auto session = m_sessionFactory(*ep);
-        if (!session)
-        {
-            std::cout << "session declined\n";
-            doAccept();
-            return;
-        }
+        auto session = std::make_shared<WebSocketSession>();
 
         std::shared_ptr<SessionOwnerBase> owner;
         owner = std::make_shared<SessionOwnerWS>(std::move(socket));
-        owner->setInitialServerOptions(session->getInitialOptions());
-        session->m_ioExecutorHolder = std::make_unique<ExecutorHolder>(owner->executor(), session);
+        owner->setInitialServerOptions();
+        session->m_ioExecutorHolder = std::make_unique<ExecutorHolder>(owner->executor());
         owner->setSession(std::move(session));
 
         // and initiate
@@ -683,14 +465,12 @@ public:
     tcp::acceptor m_acceptor;
 
     std::vector<std::thread> m_threads;
-
-    WebSocketSessionFactoryFunc m_sessionFactory;
 };
 
-WebSocketServer::WebSocketServer(WebSocketSessionFactoryFunc sessionFactory, uint16_t port, int numThreads)
+WebSocketServer::WebSocketServer(uint16_t port, int numThreads)
 {
     auto const address = tcp::v4();
-    m_server.reset(new Server(std::move(sessionFactory), tcp::endpoint(address, port), numThreads));
+    m_server.reset(new Server(tcp::endpoint(address, port), numThreads));
 }
 
 WebSocketServer::~WebSocketServer() = default;
@@ -721,17 +501,9 @@ void client(const std::string msg, const int num, const int ms)
     ws.close(websocket::close_code::normal);
 }
 
-struct ReceiverSession final : public WebSocketSession
-{};
-
-WebSocketSessionPtr Make_ReceiverSession(const WebSocketEndpointInfo&)
-{
-    return std::make_shared<ReceiverSession>();
-}
-
 int main()
 {
-    WebSocketServer server(Make_ReceiverSession, 7654, 2);
+    WebSocketServer server(7654, 2);
     //while (true) std::this_thread::yield;
 
     std::vector<std::thread> clients;
