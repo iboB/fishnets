3620fbcc0be6824db55995663fcb1a4b1a64bb91
Borislav Stanimirov <b@ibob.bg>
2025-02-11 14:51:09 +0200

    terminal client demo to new interface

--- example/e-TerminalClient.cpp
+++ example/e-TerminalClient.cpp
@@ -1,48 +1,40 @@
 // Copyright (c) Borislav Stanimirov
 // SPDX-License-Identifier: MIT
 //
-#include <fishnets/WebSocketClient.hpp>
-#include <fishnets/WebSocketSession.hpp>
+#include <fishnets/Context.hpp>
+#include <fishnets/util/WsSessionHandler.hpp>
 
 #include <atomic>
 #include <iostream>
 #include <queue>
 #include <thread>
-#include <optional>
 #include <cassert>
-#include <chrono>
-#include <mutex>
 
-class App;
 
-class Session final : public fishnets::WebSocketSession, public std::enable_shared_from_this<Session>
+class Session final : public fishnets::WsSessionHandler
 {
 public:
-    Session(App& app) : m_app(app) {}
+    std::atomic_bool active = true;
 
-    void send(std::string text)
-    {
-        postWSIOTask([this, text = std::move(text)]() mutable {
-            onSend(std::move(text));
+    void send(std::string text) {
+        postWsIoTask([this, text = std::move(text)]() mutable {
+            if (text == "/exit") {
+                wsClose();
+            }
+            else {
+                onSend(std::move(text));
+            }
         });
     }
 
-    void disconnect()
-    {
-        postWSIOTask([this]() {
-            wsClose();
-        });
-    }
 private:
-    void onSend(std::string text)
-    {
+    void onSend(std::string text) {
         m_queue.emplace(std::move(text));
-        if (m_curPacket) return; // we alrady have stuff going on
+        if (m_curPacket) return; // we already have stuff going on
         sendNext();
     }
 
-    void sendNext()
-    {
+    void sendNext() {
         assert(!m_queue.empty());
         assert(!m_curPacket);
         m_curPacket.emplace(std::move(m_queue.front()));
@@ -50,127 +42,53 @@ private:
         wsSend(*m_curPacket);
     }
 
-    itlib::span<uint8_t> wsOpened() override; // defined below as it depends on the app
-
-    itlib::span<uint8_t> wsReceivedBinary(itlib::span<uint8_t> binary, bool) override
-    {
-        std::cout << "Received binary with size " << binary.size() << '\n';
-        return {};
+    void wsOpened(std::string_view) override {
+        wsReceive();
     }
 
-    itlib::span<uint8_t> wsReceivedText(itlib::span<char> text, bool) override
-    {
+    void wsClosed(std::string reason) override {
+        std::cout << "Connection closed: " << reason << '\n';
+        active = false;
+    }
+
+    void wsReceivedBinary(itlib::span<uint8_t> binary, bool) override {
+        std::cout << "Received binary with size " << binary.size() << '\n';
+        wsReceive();
+    }
+
+    void wsReceivedText(itlib::span<char> text, bool) override {
         std::string_view str(text.data(), text.size());
         std::cout << "Received text " << str << '\n';
-        return {};
+        wsReceive();
     }
 
-    void wsCompletedSend() override
-    {
+    void wsCompletedSend() override {
         m_curPacket.reset();
         if (m_queue.empty()) return; // nothing to do
         sendNext();
     }
 
-    App& m_app;
-
     std::queue<std::string> m_queue;
     std::optional<std::string> m_curPacket;
 };
 
-using SessionPtr = std::shared_ptr<Session>;
-
-class App
-{
-public:
-    App()
-    {
-        m_client.emplace([this](const fishnets::WebSocketEndpointInfo&) {
-            return std::make_shared<Session>(*this);
-        });
-        m_serverConnectionThread = std::thread([this]() { runServerConnectionThread(); });
-    }
-
-    // any thread
-    bool active() const { return m_active.load(std::memory_order_acquire); }
-
-    void sendCommand(std::string cmd)
-    {
-        if (cmd == "/quit")
-        {
-            m_active.store(false, std::memory_order_release);
-            m_client->stop();
-            m_serverConnectionThread.join();
-            std::cout << "App has shut down. Press <ENTER> to quit.\n";
-            return;
-        }
-
-        std::lock_guard l(m_sessionMutex);
-        if (!m_currentConnectedSession)
-        {
-            std::cout << "No session is currently connected\n";
-        }
-        else
-        {
-            m_currentConnectedSession->send(std::move(cmd));
-        }
-    }
-
-    void sessionConnected(SessionPtr session)
-    {
-        std::cout << "Conection to the server was established\n";
-        std::lock_guard l(m_sessionMutex);
-        m_currentConnectedSession = session;
-    }
-
-private:
-
-    void onSessionDisconnected()
-    {
-        std::lock_guard l(m_sessionMutex);
-        m_currentConnectedSession.reset();
-    }
-
-    // connection thrad
-    void runServerConnectionThread()
-    {
-        while (active())
-        {
-            m_client->connect("localhost", 7654);
-            onSessionDisconnected();
-            std::cout << "Disconnected. Trying to reconnect...\n";
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            m_client->restart();
-        }
-    }
-
-private:
-    std::atomic_bool m_active = true;
-
-    std::optional<fishnets::WebSocketClient> m_client;
-
-    std::mutex m_sessionMutex;
-    SessionPtr m_currentConnectedSession;
-
-    std::thread m_serverConnectionThread;
-};
-
-itlib::span<uint8_t> Session::wsOpened()
-{
-    m_app.sessionConnected(shared_from_this());
-    return {};
-}
-
 int main()
 {
-    App app;
+    fishnets::Context ctx;
+    auto session = std::make_shared<Session>();
 
-    while (app.active())
-    {
+    ctx.wsConnect(session, "ws://localhost:7654");
+
+    std::thread t([&ctx] {
+        ctx.run();
+    });
+
+    while (session->active) {
         std::string cmd;
         std::getline(std::cin, cmd);
-        app.sendCommand(std::move(cmd));
+        session->send(std::move(cmd));
     }
 
+    t.join();
     return 0;
 }
