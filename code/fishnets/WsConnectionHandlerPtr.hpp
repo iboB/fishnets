cafc2b34ff269729e9e7c1eb10342236c25fda42
Borislav Stanimirov <b@ibob.bg>
2025-09-20 06:58:42 +0300

    make life easy when declaring conn handler factories

--- code/fishnets/WsConnectionHandlerPtr.hpp
+++ code/fishnets/WsConnectionHandlerPtr.hpp
@@ -8,4 +8,8 @@
 namespace fishnets {
 class WsConnectionHandler;
 using WsConnectionHandlerPtr = std::shared_ptr<WsConnectionHandler>;
+
+// this is not really needed here per se,
+// but it's useful if you just want to forward declare connection handler factories
+struct EndpointInfo;
 } // namespace fishnets
