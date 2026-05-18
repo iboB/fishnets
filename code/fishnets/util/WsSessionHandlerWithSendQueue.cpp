d3d5a9b9c391dcde27449a706eb452004d00d476
Borislav Stanimirov <b@ibob.bg>
2026-05-18 13:05:51 +0300

    minor comments

--- code/fishnets/util/WsSessionHandlerWithSendQueue.cpp
+++ code/fishnets/util/WsSessionHandlerWithSendQueue.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
 #include "WsSessionHandlerWithSendQueue.hpp"
 
 namespace fishnets {
--- code/fishnets/util/WsSessionHandlerWithSendQueue.hpp
+++ code/fishnets/util/WsSessionHandlerWithSendQueue.hpp
@@ -13,19 +13,20 @@ namespace fishnets {
 
 class FISHNETS_API WsSessionHandlerWithSendQueue : public WsSessionHandler {
 protected:
-    void wsAddToSendQueue(std::string text);
-
+    // add items to send queue
+    // as all ws* functions, only valid on the corresponding io strand
     using BinaryBufferType = itlib::pod_vector<std::byte>;
+    void wsAddToSendQueue(std::string text);
     void wsAddToSendQueue(BinaryBufferType binary);
+
 private:
-    using WsSessionHandler::wsSend;
+    using WsSessionHandler::wsSend; // hide from direct callers
     virtual void wsCompletedSend() final override;
 
     using Item = std::variant<
         std::string, // text
         BinaryBufferType  // binary
     >;
-
     std::queue<Item> m_queue;
     std::optional<Item> m_curItem;
 
